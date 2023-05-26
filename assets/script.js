1ac72023ef6e693f0c911e8d0f5f3d9ed5fbd3f9
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-05-25 22:58:32 -0400

    Added functionality in javascript

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,14 @@
+var searchEl = document.getElementById("search-button");
+
 var cityName = document.getElementById("City")
-cityName.addEventListener("click", getApi);
-console.log(cityName)
 
-var weather = document.getElementById('forecast');
+var weather = document.getElementById('weather');
+var forecast = document.getElementById('Forecast');
+var apiKey = "15ce8a000d8c17fd6d6c80ca37333433"
 
-function getApi() {
-    var requestUrl = "api.openweathermap.org/data/2.5/forecast?q=newyork&appid=15ce8a000d8c17fd6d6c80ca37333433"
+function getForecast(city) {
+    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+    console.log(city)
 
     fetch(requestUrl)
     .then(function (reponse) {
@@ -14,11 +17,55 @@ function getApi() {
     .then(function (data) {
         console.log(data)
 
-        for (var i = 0; i < data.length; i++) {
-            var listItem= document.createElement('li');
-            listItem.textContent = data[i].html_url;
-            weather.appendChild(listItem);
+        for (var i = 0; i < data.list.length; i+=8) {
+            const day = data.list[i]
+            var card = document.createElement('div');
+            card.setAttribute ("class", "card")
+            var cardBody = document.createElement('div');
+            cardBody.setAttribute ("class", "card-body")
+            card.innerHTML = `
+            <h2>${day.dt_txt.slice(0,10)}</h2>
+            <p>weather ${day.main.temp}</p>
+            <p>humidity ${day.main.humidity}</p>
+            <p>wind ${day.wind.speed}</p>
+            `;
+            card.appendChild(cardBody);
+            forecast.appendChild(card);
             
         }
     })
 }
+
+function getWeather(city) {
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    console.log(city)
+
+    fetch(requestUrl)
+    .then(function (reponse) {
+        return reponse.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        const day = data
+            var card = document.createElement('div');
+            card.setAttribute ("class", "card current-weather")
+            var cardBody = document.createElement('div');
+            cardBody.setAttribute ("class", "card-body")
+            card.innerHTML = `
+            <p>weather ${day.main.temp}</p>
+            <p>humidity ${day.main.humidity}</p>
+            <p>wind ${day.wind.speed}</p>
+            `;
+            card.appendChild(cardBody);
+            weather.appendChild(card);
+            getForecast(city)
+            
+        
+    })
+}
+
+searchEl.addEventListener("click", function () {
+    getWeather(cityName.value)
+
+})
+
